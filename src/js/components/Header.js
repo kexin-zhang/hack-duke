056094ef1ef4dc3d88017b2b5b75320552066c3a
davidjtran <david.tran456@gmail.com>
2016-11-20 13:11:11 -0500

    UI tweaks

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -4,7 +4,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <h1>
-        <img class="logo" src="../../images/logo.png" alt="logo" height="50" width="200"></img>
+        <img class="logo" src="../../images/logo.png" alt="logo"></img>
       </h1>
     );
   }
