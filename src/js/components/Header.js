c0a7339bf89e71f5e7d8e59bb530de66f087afd7
davidjtran <david.tran456@gmail.com>
2016-11-20 12:53:05 -0500

    Logo added

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 export default class Header extends React.Component {
   render() {
     return (
-      <h1> move up! </h1>
+      <h1>
+        <img class="logo" src="../../images/logo.png" alt="logo" height="50" width="200"></img>
+      </h1>
     );
   }
 }
